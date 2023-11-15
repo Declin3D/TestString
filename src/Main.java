febf8c5443bb76c4f365c3044cfa6057adf16152
Declin3D <rdelektaa@o2.pl>
2023-11-15 09:35:56 +0100

    zadanie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj tekst");
+        String t1 = scanner.nextLine();
+        String dwieOstatnie;
+        String reszta;
+
+        if(t1.length() >= 3){
+            dwieOstatnie = t1.substring(t1.length()-2,t1.length());
+            reszta = t1.substring(0,t1.length()-2);
+            System.out.println("Zamieniony tekst : " + dwieOstatnie + reszta);
+        }else {
+            System.out.println("Nie udało się - tekst '" + t1 + "' jest za krótki");
+        }
 
     }
 }
